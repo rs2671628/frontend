42a76a94e56d5b7b4c737a6feea92f88524fad4a
rs2671628 <rs2671628@gmail.com>
2024-06-06 01:20:16 +0530

    holo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { Outlet } from 'react-router-dom';
 import './App.css';
-
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import SummaryApi from './common';
+import Context from './context';
+import { useDispatch } from 'react-redux';
+import { setUserDetails } from './store/userSlice';
 function App() {
+  const dispatch=useDispatch()
+  const [cartProductCount,setCartProductCount]=useState(0)
+
+  const fetchUserDetails=async()=>{
+    const dataResponse=await fetch(SummaryApi.current_user.url,{
+      method:SummaryApi.current_user.method,
+      credentials:'include',
+    })
+    const dataApi= await dataResponse.json()
+    if(dataApi.success){
+      dispatch(setUserDetails(dataApi.data))
+    }
+  }
+const fetchUserAddToCart=async()=>{
+  const dataResponse=await fetch(SummaryApi.addToCardProductCount.url,{
+    method:SummaryApi.addToCardProductCount.method,
+    credentials:'include',
+  })
+  const dataApi= await dataResponse.json()
+  console.log("dataApi",dataApi)
+  setCartProductCount(dataApi?.data?.count)
+}
+
+  useEffect(()=>{
+    //USER DETAILS
+    fetchUserDetails()
+    //USER DETAILS CART PRODUCT
+    fetchUserAddToCart()
+
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Context.Provider value={{
+      fetchUserDetails,//user details fetch
+      cartProductCount, //current user add to cart product count
+      fetchUserAddToCart
+  }}>
+    <ToastContainer />
+    <Header/>
+    <main className='min-h-[calc(100vh-120px)] pt-16'><Outlet/></main>
+    <Footer/>
+    </Context.Provider>
+    </>
   );
 }
 
